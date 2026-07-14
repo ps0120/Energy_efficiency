38ced64090107d09c5d55c9df72a91da2fdf0a76
Su Ping Shun <1211210778@student.mmu.edu.my>
2026-07-14 23:19:01 +0800

    done

--- Energy/command.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system("start /wait cmd /c python Program.py")
-
-#subprocess.Popen('cmd.exe /K netstat') # run cmd commands like netstat,..etc
-#subprocess.Popen('cmd.exe /K python') # open cmd in  Python live interpreter mode
-#subprocess.Popen('cmd.exe /K my_script.py') # run your python script
